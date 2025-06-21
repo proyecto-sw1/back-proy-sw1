a30836ea0d9be76908b839b183285d127c289b7c
RafaelSvgh <rafaplaying@gmail.com>
2025-06-21 16:27:53 -0400

    return user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -49,10 +49,10 @@ export class AuthService {
     const payload = { id: user.id, email: user.email };
 
     const token = await this.jwtService.signAsync(payload);
-
+    const { password: _, ...userWithoutPassword } = user;
     return {
       token,
-      email,
+      user: userWithoutPassword,
     };
   }
 
